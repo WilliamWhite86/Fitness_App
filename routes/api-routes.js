27e5efee3ca6b851abc143687ccbe7b5f39119c2
William White <william.white86@gmail.com>
2020-01-31 22:12:07 -0500

    got front end rendering

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,38 @@
-// getLastWorkout() GET /api/workouts
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
 
+// Dependencies
+// =============================================================
+
+// Requiring our Workout and Index models
+const db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the workouts
+//   app.get("/workouts/", function(req, res) {
+//     db.Workout.findAll({})
+//       .then(function(dbWorkout) {
+//         console.log(res);
+//         res.json(dbWorkout);
+//       });
+//   })};
+
+
+// getLastWorkout() GET /api/workouts
+app.get("/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  };  
 
 
 
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require('./routes');
+// const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
 
 const app = express();
 const databaseName = 'workout_db';
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use('/', routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
   useNewUrlParser: true,
