ceec6b1b94d4bf9e51b2ed51f99b1a15095f2211
William White <william.white86@gmail.com>
2020-02-02 18:54:25 -0500

    added local host as seed connect option

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
